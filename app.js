0ed378e4d478f1b4fe205103d3e698fb24bd0f5a
pup-uno <jess.rodrguez8@gmail.com>
2020-05-23 15:15:36 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,52 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+// Funcion de las imagenes de los certidicados
+
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg").addEventListener('click', OpenModal);
+var img = document.getElementById("myImgDos").addEventListener('click', OpenModal);
+var img = document.getElementById("myImgTres").addEventListener('click', OpenModal);
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+function OpenModal(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
